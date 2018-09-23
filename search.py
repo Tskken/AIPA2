b7c279709aa77899b25474b730a0a327148b0630
Sloan Anderson <35469656+Hellboy632789@users.noreply.github.com>
2018-09-23 15:33:57 -0400

    Update search.py

--- search.py
+++ search.py
@@ -148,8 +148,35 @@ def a_star_search(problem, heuristic=null_heuristic):
 
     A* searches the node that has the lowest combined cost and heuristic first.
     """
-    # *** YOUR CODE HERE ***
-    util.raise_not_defined()
+    # Create closed node list with the start state as its first item
+    closed_list = [problem.get_start_state()]
+    # Expand start state and add its children to the fringe
+    # Note: Fringe structure is a list of 2 items, [node name (ie A), list of paths that got you to that node]
+    fringe = util.PriorityQueue()
+    for first_node in problem.get_successors(problem.get_start_state()):
+        fringe.push([first_node[0], [first_node[1]], first_node[2]], null_heuristic(problem.get_start_state(),first_node[2]))
+
+    # Loop while there is still nodes to expand in the fringe
+    while not fringe.is_empty():
+        # Pop node off fringe
+        node = fringe.pop()
+        # Check if the current node is the goal state
+        if problem.is_goal_state(node[0]):
+            # Return path to goal state
+            return node[1]
+        # Check if node is in closed list
+        if node[0] not in closed_list:
+            # Add node to closed list
+            closed_list.append(node[0])
+            # Expand node adding its children to the fringe
+            for nextNode in problem.get_successors(node[0]):
+                # Copy path from past node
+                path = node[1][:]
+                # Add next nodes path to list
+                path.append(nextNode[1])
+                nCost = problem.get_cost_of_actions(path) + heuristic(node[0], problem)
+                # Add node to fringe
+                fringe.push([nextNode[0], path], nCost)
 
 
 # Abbreviations
